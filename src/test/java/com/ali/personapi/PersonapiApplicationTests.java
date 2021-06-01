2d3991ce763bdf0780b504d65dc1aa2709c31076
ALI IDDAR <ali-iddar@redes.ufsm.br>
2021-06-01 17:31:31 -0300

    Starting the project

---
+++ src/test/java/com/ali/personapi/PersonapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ali.personapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
