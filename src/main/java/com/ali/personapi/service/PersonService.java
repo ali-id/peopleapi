57ff2ed57767b23ab1f51398b4990267cee3563c
ALI IDDAR <ali-iddar@redes.ufsm.br>
2021-06-03 11:31:11 -0300

    List all method developed

--- src/main/java/com/ali/personapi/service/PersonService.java
+++ src/main/java/com/ali/personapi/service/PersonService.java
@@ -9,6 +9,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import javax.validation.Valid;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class PersonService {
@@ -30,5 +32,13 @@ public class PersonService {
                 .builder()
                 .message("Created person with ID" + savePerson.getId())
                 .build();
+    }
+
+
+    public List<PersonDTO> listAll() {
+        List<Person> allPeople = personRepository.findAll();
+        return allPeople.stream()
+                .map(personMapper::toDTO)
+                .collect(Collectors.toList());
     }
 }
