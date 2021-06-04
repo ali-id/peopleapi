3a8ff21331dd0a21c42bcbe17c11eb0663d07016
ALI IDDAR <ali-iddar@redes.ufsm.br>
2021-06-04 18:01:12 -0300

    README.md file created

--- src/main/java/com/ali/personapi/service/PersonService.java
+++ src/main/java/com/ali/personapi/service/PersonService.java
@@ -6,6 +6,7 @@ import com.ali.personapi.entity.Person;
 import com.ali.personapi.exception.PersonNotFoundException;
 import com.ali.personapi.mapper.PersonMapper;
 import com.ali.personapi.repository.PersonRepository;
+import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -14,16 +15,12 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 @Service
+@AllArgsConstructor(onConstructor = @__(@Autowired))
 public class PersonService {
 
     private PersonRepository personRepository;
 
     private final PersonMapper personMapper = PersonMapper.INSTANCE;
-
-    @Autowired
-    public PersonService(PersonRepository personRepository) {
-        this.personRepository = personRepository;
-    }
 
     public MessageResponseDTO createPerson(@Valid PersonDTO personDTO){
         Person personToSave = personMapper.toModel(personDTO);
